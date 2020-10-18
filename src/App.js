dde2d0825797129f693daf00936abe4955b37de3
Zaki Rangwala <zakirangwala@gmail.com>
2020-10-18 13:07:39 -0400

    add more categories

--- src/App.js
+++ src/App.js
@@ -12,6 +12,12 @@ function App() {
         fetchUrl={requests.fetchNetflixOriginals}
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Indian Movies" fetchUrl={requests.fetchIndianMovies} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
     </div>
   );
 }
--- src/requests.js
+++ src/requests.js
@@ -8,5 +8,6 @@ const requests = {
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchIndianMovies : `discover/movie?api_key=${API_KEY}&language=hi-IN&region=IN&sort_by=popularity.desc&page=1&primary_release_year=2018&with_original_language=hi`
 };
 export default requests;

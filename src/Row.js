5a97a9b3c6a728d774ffd375e6cf211a9495dfe5
Zaki Rangwala <zakirangwala@gmail.com>
2020-10-18 12:08:06 -0400

    add movie and images from tmdb api

---
+++ src/Row.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  //Snippet that runs based on a specific condition or variable
+  useEffect(() => {
+    // will make asynchronous call since info coming from 3rd party service (1 to 3 seconds)
+    //make requests to TMDB
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]); //if [] is blank, run once when row loads and dont run again
+  console.log(movies);
+  return (
+    <div className="row">
+      {/* title */}
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {/* several row_posters */}
+        {movies.map((movie) => (
+          <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+        ))}
+      </div>
+      {/* container -> posters */}
+    </div>
+  );
+}
+export default Row;

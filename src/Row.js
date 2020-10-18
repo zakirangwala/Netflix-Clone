7a923e01953cd7ee451598718b84516bd1af2f83
Zaki Rangwala <zakirangwala@gmail.com>
2020-10-18 16:19:48 -0400

    make pics wider, add banner

--- src/Row.js
+++ src/Row.js
@@ -3,7 +3,7 @@ import axios from "./axios";
 import "./Row.css";
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   //Snippet that runs based on a specific condition or variable
@@ -17,7 +17,7 @@ function Row({ title, fetchUrl }) {
     }
     fetchData();
   }, [fetchUrl]); //if [] is blank, run once when row loads and dont run again
-  console.log(movies);
+  // console.table(movies);
   return (
     <div className="row">
       {/* title */}
@@ -27,8 +27,10 @@ function Row({ title, fetchUrl }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
